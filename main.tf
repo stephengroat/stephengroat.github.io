463a544ceb5a17c3e4eb7da9dbd029ab44ff3176
Stephen <stephengroat@users.noreply.github.com>
2020-08-26 13:54:36 -0700

    add datadog synthetics tests

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
 provider datadog {}
+
+resource "datadog_synthetics_test" "test_browser" {
+  type = "browser"
+  
+  name = "www.stephengroat.com"
+  message = "@stephengroat@gmail.com"
+  tags    = []
+
+  status = "live"
+
+  request = {
+    method = "HEAD"
+    url    = "http://www.stephengroat.com"
+  }
+
+  locations  = ["aws:eu-central-1", "aws:us-east-2"]
+
+  options = {
+    follow_redirects = true
+    tick_every = 43200
+  }
+}
